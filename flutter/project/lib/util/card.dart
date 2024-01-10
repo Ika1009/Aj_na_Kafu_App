50ae311f5508653b1eda731e053b26fe06127c57
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-01-10 14:08:28 +0100

    Refactor UI layout for better image integration
    Updated the CustomCard widget to improve visual appeal by refactoring
    the layout structure. Introduced a stack to overlay UI elements on a
    full-height background image, ensuring a more immersive experience.
    Adjusted text alignment for better readability and enhanced the
    aesthetic with rounded corners on the overlay. Removed the AppBar to
    streamline navigation elements directly onto the image background for a
    cleaner look. These changes aim to provide users with a more engaging
    and cohesive design that leverages the full-screen image to capture
    attention.

--- flutter/project/lib/util/card.dart
+++ flutter/project/lib/util/card.dart
@@ -3,58 +3,74 @@ import 'package:flutter/material.dart';
 class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double width80Percent = MediaQuery.of(context).size.width * 0.8;
+    double appBarHeight = MediaQuery.of(context).size.height * 0.4; 
+    double width80Percent = MediaQuery.of(context).size.width * 0.8; 
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: EdgeInsets.only(top: 24, left: 15), // Adjust top and left padding as needed
-            child: IconButton(
-              icon: Icon(Icons.arrow_back_ios),
-              iconSize: 16, // Smaller icon size
-              onPressed: () {
-                Navigator.maybePop(context);
-              },
-            ),
-          ),
-        ),
-        flexibleSpace: Image.asset(
-          'assets/images/room.jpg', // Replace with your image path
-          fit: BoxFit.cover,
-        ),
-        toolbarHeight: 250, // Adjust as needed
-        elevation: 0,
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFF0C3B2E),  // Assuming a white background
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(40.0),
-            topRight: Radius.circular(40.0),
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Center vertically
-          children: [
-            Container(
-              width: double.infinity, // Full width of the screen
-              child: Column(
-                children: [
-                  Container(
-                    width: width80Percent,
-                    child: Text(
-                      "Miami St beach front, Myrtle Beach at the resort and hotel in front of GC Lab.",
-                      style: TextStyle(color: Colors.white),
-                      textAlign: TextAlign.center,
+      body: Column( 
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/room.jpg'),
+                    alignment: Alignment.topCenter,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: appBarHeight - 50, 
+                left: 0,
+                right: 0,
+                child: Container(
+                  padding: EdgeInsets.only(top: 40), 
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0C3B2E),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(35.0),
+                      topRight: Radius.circular(35.0),
                     ),
                   ),
-                ],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height,
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: width80Percent,
+                              child: Text(
+                                "Miami St beach front, Myrtle Beach at the resort and hotel in front of GC Lab.",
+                                style: TextStyle(color: Colors.white),
+                                textAlign: TextAlign.justify,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
+              Positioned(
+                top: 30, 
+                left: 20, 
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  iconSize: 16, 
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.maybePop(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
