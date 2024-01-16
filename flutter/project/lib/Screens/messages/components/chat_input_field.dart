1b26a5549f83cfc4b81385618748070b125bf065
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-01-16 02:02:22 +0100

    Refine chat input field UI
    Simplify the chat input field by removing icons for microphone, emoji,
    attachment, and camera, and adjusting the padding and decoration of the
    input container. Improve focus on text entry by aligning text to the
    start and adding a send icon. This change aims to streamline the chat
    interface for better user experience by reducing clutter and emphasizing
    message composition.

--- flutter/project/lib/Screens/messages/components/chat_input_field.dart
+++ flutter/project/lib/Screens/messages/components/chat_input_field.dart
@@ -27,53 +27,21 @@ class ChatInputField extends StatelessWidget {
       child: SafeArea(
         child: Row(
           children: [
-            const Icon(Icons.mic, color: primaryColor),
-            const SizedBox(width: kDefaultPadding),
             Expanded(
               child: Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: kDefaultPadding * 0.75,
-                ),
-                decoration: BoxDecoration(
-                  color: primaryColor.withOpacity(0.05),
-                  borderRadius: BorderRadius.circular(40),
-                ),
                 child: Row(
                   children: [
-                    Icon(
-                      Icons.sentiment_satisfied_alt_outlined,
-                      color: Theme.of(context)
-                          .textTheme
-                          .bodyLarge!
-                          .color!
-                          .withOpacity(0.64),
-                    ),
-                    const SizedBox(width: kDefaultPadding / 4),
                     const Expanded(
                       child: TextField(
+                        textAlign: TextAlign.start,
                         decoration: InputDecoration(
                           hintText: "Type message",
                           border: InputBorder.none,
                         ),
                       ),
                     ),
-                    Icon(
-                      Icons.attach_file,
-                      color: Theme.of(context)
-                          .textTheme
-                          .bodyLarge!
-                          .color!
-                          .withOpacity(0.64),
-                    ),
-                    const SizedBox(width: kDefaultPadding / 4),
-                    Icon(
-                      Icons.camera_alt_outlined,
-                      color: Theme.of(context)
-                          .textTheme
-                          .bodyLarge!
-                          .color!
-                          .withOpacity(0.64),
-                    ),
+                    const SizedBox(width: kDefaultPadding / 2),
+                    Icon(Icons.arrow_forward, color: primaryColor),
                   ],
                 ),
               ),
