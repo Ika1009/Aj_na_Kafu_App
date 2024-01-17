a29cfc70b939b60e7121254655df3a0960049d67
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-01-17 22:09:07 +0100

    Refactor launch screen with branding and navigation
    Upgraded the LaunchScreen to offer users a more engaging starting point
    by integrating a new brand look and direct navigation option. The
    redesign features the app's logo, a captivating tagline, and a custom
    floating action button (FAB) location for improved user experience. The
    custom FAB facilitates easy transition to the SignInScreen, boosting
    user flow efficiency. Enhanced visual appeal aligns with the brand
    identity and aims to increase user retention right from launch.

--- flutter/project/lib/Screens/launch/launch_screen.dart
+++ flutter/project/lib/Screens/launch/launch_screen.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project/components/background.dart';
+import 'package:project/constants.dart';
+import 'package:project/screens/signin/signin_screen.dart';
+
+class CustomFabLocation extends FloatingActionButtonLocation {
+  final FloatingActionButtonLocation location;
+  final double offsetY;
+
+  const CustomFabLocation(this.location, this.offsetY);
+
+  @override
+  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+    final Offset originalOffset = location.getOffset(scaffoldGeometry);
+    return Offset(originalOffset.dx, originalOffset.dy - offsetY);
+  }
+}
 
 class LaunchScreen extends StatelessWidget {
   static String routeName = "/launch";
@@ -8,9 +23,45 @@ class LaunchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Background(
-      child: Center(
-        child: SvgPicture.asset('assets/icons/logo.svg'),
+    return Scaffold(
+      body: Background(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset('assets/icons/logo.svg'),
+              Text(
+                'Aj Na Kafu',
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: -0.80,
+                ),
+              ),
+              Text(
+                'Kod Vas ili kod nas?',
+                style: TextStyle(
+                  color: Color(0xFF757575), // dzektor da doda boju
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, SignInScreen.routeName);
+        },
+        child: Icon(Icons.arrow_downward), 
+        backgroundColor: accentColor, 
+      ),
+      floatingActionButtonLocation: CustomFabLocation(
+        FloatingActionButtonLocation.centerFloat, 
+        80,
       ),
     );
   }
