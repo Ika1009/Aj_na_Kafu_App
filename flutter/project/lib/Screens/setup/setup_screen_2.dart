1bf19fa5c7b63eb1f66f4cca792c81cf90b84ba0
Milos Mitrovic <milosmitrovic005@gmail.com>
2024-01-20 23:27:00 +0100

    Refactor setup flow using PageView
    Replace custom form and background elements in the account setup process
    with modular components for a cleaner, more maintainable codebase.
    Removed redundant and hard-coded text fields, replacing them with a
    PageView to simplify navigation between setup stages. Also, due to these
    changes, old setup_screen_3.dart was removed, and related imports and
    routes were updated. This streamlines the user experience by
    facilitating a smooth slide transition between setup screens.

--- flutter/project/lib/Screens/setup/setup_screen_2.dart
+++ flutter/project/lib/Screens/setup/setup_screen_2.dart
@@ -1,160 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:project/backgrounds/background_setup_2.dart';
-
-import '../../constants.dart';
-import 'setup_screen_3.dart';
+import 'package:project/screens/setup/components/setup_component.dart';
+import 'package:project/screens/setup/components/setup_component_2.dart';
+import 'package:project/screens/setup/components/setup_component_3.dart';
 
 class AccountSetupScreen extends StatelessWidget {
   static String routeName = "/account_setup";
-  const AccountSetupScreen({super.key});
+  AccountSetupScreen({super.key});
+  final _controller = PageController();
 
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments;
-    final String username = args is String ? args : 'User';
     return Scaffold(
-      body: AccountSetupBackground(
-        progressText: '1/3',
-        progressValue: 1/3,
-        child: Container(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hello, $username!',
-                style: const TextStyle(
-                  color: primaryColor,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  letterSpacing: -0.24,
-                ),
-              ),
-              const Text(
-                'Great, just few more steps...',
-                style: TextStyle(
-                  color: Color(0xFF757575), // dzektor da doda boju
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: defaultPadding * 4),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.next,
-                cursorColor: primaryColor,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color(0xFFD6E6DA), // dzektor da doda boju i da se zameni
-                  hintText: "First Name",
-                  hintStyle: const TextStyle(
-                    color: Color(0xFF757575), // dzektor da doda boju i da se zameni
-                    fontWeight: FontWeight.w600,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                    borderSide: BorderSide.none, 
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                    borderSide: BorderSide.none, 
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                    borderSide: BorderSide.none, 
-                  ),
-                ),
-              ),
-              const SizedBox(height: defaultPadding / 2),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-                child: TextFormField(
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.next,
-                  cursorColor: primaryColor,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: const Color(0xFFD6E6DA), // dzektor da doda boju i da se zameni
-                    hintText: "Last Name",
-                    hintStyle: const TextStyle(
-                      color: Color(0xFF757575), // dzektor da doda boju i da se zameni
-                      fontWeight: FontWeight.w600,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25), 
-                      borderSide: BorderSide.none, 
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25), 
-                      borderSide: BorderSide.none, 
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25), 
-                      borderSide: BorderSide.none, 
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: defaultPadding / 2),
-              TextFormField(
-                keyboardType: TextInputType.phone,
-                textInputAction: TextInputAction.done,
-                cursorColor: primaryColor,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: const Color(0xFFD6E6DA), // dzektor da doda boju i da se zameni
-                  hintText: "Phone Number",
-                  hintStyle: const TextStyle(
-                    color: Color(0xFF757575), // dzektor da doda boju i da se zameni
-                    fontWeight: FontWeight.w600,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                    borderSide: BorderSide.none, 
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                    borderSide: BorderSide.none, 
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25), 
-                    borderSide: BorderSide.none, 
-                  ),
-                ),
-              ),
-              const SizedBox(height: defaultPadding * 4),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(backgroundColor),
-                  minimumSize: MaterialStateProperty.all(const Size(double.infinity, 62)),
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25), 
-                      side: const BorderSide(color: accentColor, width: 2.0), 
-                    ),
-                  ),
-                ),
-                onPressed: ()  {
-                  Navigator.pushNamed(
-                    context,
-                    AccountSetupScreen2.routeName,
-                    arguments: username,
-                  );
-                },
-                child: const Text(
-                  "Continue",
-                  style: TextStyle(
-                    color: accentColor,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: PageView(
+        controller: _controller,
+        children: const [
+          AccountSetup(),
+          AccountSetup2(),
+          AccountSetup3(),
+        ],
       ),
     );
   }
