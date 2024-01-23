90410f81a11a75e7947e05add8dffcfd2574ab6b
Ika1009 <ilija.doncic@yahoo.com>
2024-01-23 19:10:32 +0100

    kod za ispisvanje korisnika

--- flutter/project/lib/Screens/chats/components/body.dart
+++ flutter/project/lib/Screens/chats/components/body.dart
@@ -1,6 +1,9 @@
 import 'package:project/models/Chat.dart';
 import 'package:project/screens/messages/message_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 import 'chat_card.dart';
 
@@ -29,3 +32,37 @@ class Body extends StatelessWidget {
     );
   }
 }
+
+// evo ti kod da se napravi lista korisnika koji treba se ispisu ovde
+// Widget _buildUserList() {
+//   return StreamBuilder<QuerySnapshot>(
+//     stream: FirebaseFirestore.instance.collection('users').snapshots(),
+//     builder: (context, snapshot) {
+//       if (snapshot.hasError) {
+//         return const Text('Error');
+//       }
+//       if (snapshot.connectionState == ConnectionState.waiting) {
+//         return const Text('Loading...');
+//       }
+//       return ListView(
+//         children: snapshot.data!.docs
+//             .map<Widget>((doc) => _buildUserListItem(doc))
+//             .toList(),
+//       );
+//     },
+//   );
+// }
+
+// Widget _buildUserListItem(DocumentSnapshot document) {
+//   Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+//   // Assuming _auth is an instance of FirebaseAuth
+//   if (FirebaseAuth.instance.currentUser!.email != data['email']) {
+//     return ListTile(
+//       title: Text(data['email']),
+//       onTap: () { // ovde treba da otvori stranu za chatovanje sa UID }
+//     );
+//   } else {
+//     return Container(); // Return an empty container if it's the current user
+//   }
+// }
