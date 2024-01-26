2333fd5c358d3f598d5c5c4d220e04c691f40ec6
Ika1009 <ilija.doncic@yahoo.com>
2024-01-26 14:12:20 +0100

    evo kod za profilne slike

--- flutter/project/lib/services/auth_service.dart
+++ flutter/project/lib/services/auth_service.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:image_input/image_input.dart';
 
 class AuthService extends ChangeNotifier {
   // Instance of FirebaseAuth
@@ -32,7 +36,7 @@ class AuthService extends ChangeNotifier {
     String lastName,
     String dateOfBirth,
     String phoneNumber,
-    String description
+    String description,
   ) async {
     try {
       UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
@@ -49,6 +53,7 @@ class AuthService extends ChangeNotifier {
         'dateOfBirth': dateOfBirth,
         'phoneNumber': phoneNumber,
         'description': description,
+        'imageUrl': 'https://bonanza.mycpanel.rs/ajnakafu/images/profile_basic.png', // Add imageUrl to the user document
         'friends': [] // Initialize an empty friends list
       });
       return userCredential;
@@ -57,8 +62,56 @@ class AuthService extends ChangeNotifier {
     }
   }
 
+
   // Sign user out
   Future<void> signOut() async {
     return await _firebaseAuth.signOut();
   }
+
+  Future<String> uploadImage(XFile imageFile, String userUID) async {
+    final uri = Uri.parse('https://bonanza.mycpanel.rs/ajnakafu/upload_image.php'); //Cuvam sliku na nasem serveru i vracam url
+
+    try {
+      final request = http.MultipartRequest('POST', uri);
+      
+      // Add the image file to the request
+      final fileStream = http.ByteStream(imageFile.openRead());
+      final length = await imageFile.length();
+      final mimeType = MediaType('image', 'jpeg'); // Adjust the mime type as needed
+      
+      final multipartFile = http.MultipartFile('file', fileStream, length, filename: userUID, contentType: mimeType);
+      
+      request.files.add(multipartFile);
+      
+      // Send the request and get the response
+      final response = await request.send();
+      
+      if (response.statusCode == 200) {
+        // Image uploaded successfully, parse the response to get the image URL
+        final responseData = await response.stream.toBytes();
+        final responseString = String.fromCharCodes(responseData);
+        final imageUrl = json.decode(responseString)['imageUrl'];
+        return imageUrl;
+      } else {
+        throw Exception('Image upload failed with status ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Image upload failed: $e');
+    }
+  }
+
+  Future<void> updateUserProfileImage(String userUID, String imageUrl) async {
+    try {
+      // Reference to the Firestore document for the user
+      DocumentReference userDocRef = _firestore.collection('users').doc(userUID);
+
+      // Update the user document with the new image URL
+      await userDocRef.update({
+        'imageUrl': imageUrl,
+      });
+    } catch (e) {
+      throw Exception('Failed to update user profile image: $e');
+    }
+  }
 }
+
