5402e6a4ab660da3ee6830323883e356e22b2a37
Ika1009 <ilija.doncic@yahoo.com>
2024-01-26 15:16:14 +0100

    print komentar

--- flutter/project/lib/services/auth_service.dart
+++ flutter/project/lib/services/auth_service.dart
@@ -58,6 +58,7 @@ class AuthService extends ChangeNotifier {
       });
       return userCredential;
     } on FirebaseAuthException catch (e) {
+      print("email" + email);
       throw Exception(e.code);
     }
   }
