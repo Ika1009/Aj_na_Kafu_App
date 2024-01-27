c2cfe12eb99ce723160094957d357b3cdee0abff
Ika1009 <ilija.doncic@yahoo.com>
2024-01-27 17:10:05 +0100

    svi propertiji u signup

--- flutter/project/lib/services/current_user_service.dart
+++ flutter/project/lib/services/current_user_service.dart
@@ -1,6 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:project/models/user_data.dart';
+import 'package:project/services/auth_service.dart';
 
 class UserService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -42,6 +45,36 @@ class UserService {
     } catch (e) {
       //print('Error retrieving user data: $e');
       return null; // Error occurred while retrieving user data
+    }
+  }
+
+  // Update current user data
+  Future<void> updateCurrentUserData(UserData userData, Uint8List? imageData) async {
+    final userId = getCurrentUserId();
+    if (userId == null) {
+      throw Exception("User not logged in");
+    }
+
+    try {
+      await _firestore.collection('users').doc(userId).update({
+        'email': userData.email,
+        'userName': userData.userName,
+        'firstName': userData.firstName,
+        'lastName': userData.lastName,
+        'dateOfBirth': userData.dateOfBirth,
+        'phoneNumber': userData.phoneNumber,
+        'description': userData.description,
+        'username': userData.username,
+        // Add other fields as needed
+      });
+      if(imageData != null) {
+        AuthService authService = AuthService();
+        await authService.uploadImage(imageData, userId);
+      }
+    } catch (e) {
+      // Handle errors here
+      //print('Error updating user data: $e');
+      throw Exception("Error updating user data");
     }
   }
 }
