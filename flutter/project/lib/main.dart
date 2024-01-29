29ec8ec8e57c7d416a5c024a3b4c8939cf066e45
Ika1009 <ilija.doncic@yahoo.com>
2024-01-29 15:40:57 +0100

    Add Firebase options for iOS and support for it

--- flutter/project/lib/main.dart
+++ flutter/project/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:project/screens/launch/launch_screen.dart';
 import 'package:project/services/auth_service.dart';
@@ -8,14 +9,29 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FirebaseOptions firebaseOptions;
 
-  FirebaseOptions firebaseOptions = const FirebaseOptions(
-    apiKey: "AIzaSyBJ5dgytUJXtZo4qXm4Knc6j2Q_9yRvhYo",
-    appId: "1:1002141291798:android:11be0028fed83f483e6c37", 
-    messagingSenderId: "1002141291798", 
-    projectId: "ajnakafu-330fa", 
-    storageBucket: "ajnakafu-330fa.appspot.com", 
-  );
+  // Check the platform and assign the correct Firebase options
+  if (Platform.isAndroid) {
+    firebaseOptions = const FirebaseOptions(
+      apiKey: "AIzaSyBJ5dgytUJXtZo4qXm4Knc6j2Q_9yRvhYo", // Use Android apiKey
+      appId: "1:1002141291798:android:11be0028fed83f483e6c37", // Use Android appId
+      messagingSenderId: "1002141291798", // Use Android messagingSenderId
+      projectId: "ajnakafu-330fa", // Your project ID
+      storageBucket: "ajnakafu-330fa.appspot.com", // Your storage bucket
+    );
+  } else if (Platform.isIOS) {
+    firebaseOptions = const FirebaseOptions(
+      apiKey: "your-ios-apiKey", // Replace with your iOS apiKey
+      appId: "1:1002141291798:ios:c1ecf05c52a43b753e6c37", // Use iOS appId
+      messagingSenderId: "1002141291798", // Use iOS messagingSenderId
+      projectId: "ajnakafu-330fa", // Your project ID
+      storageBucket: "ajnakafu-330fa.appspot.com", // Your storage bucket
+      iosBundleId: "com.example.project", // Your iOS Bundle ID
+    );
+  } else {
+    throw UnsupportedError("This platform is not supported");
+  }
 
   await Firebase.initializeApp(options: firebaseOptions);
 
